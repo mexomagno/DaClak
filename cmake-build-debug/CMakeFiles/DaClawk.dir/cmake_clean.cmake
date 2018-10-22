4699b9d63be218c26d547ba5f3a90e9bc106d650
Maximiliano Castro <macguionbajo@gmail.com>
2018-10-21 22:04:26 -0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/DaClawk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DaClawk.dir/src/main.cpp.o"
+  "DaClawk.pdb"
+  "DaClawk"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DaClawk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by mexomagno on 21-10-18.
+//
+
+#import <Arduino.h>
+
+void setup(){
+    pinMode(13, OUTPUT);
+}
+
+void loop(){
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+}
